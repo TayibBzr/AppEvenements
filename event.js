e2030c048b3b2ed93fbf0298bf1bb553865942b0
Tayib Bouzekri <bouzekri.tayib@headn.fr>
2023-11-21 13:11:39 +0100

    Partie 5

--- event.js
+++ event.js
@@ -3,6 +3,7 @@ const reponse = await fetch(
 );
 
 const data = await reponse.json();
+const valeurDeData = data.results;
 
 let sectionFiches = document.querySelector(".fiches");
 
@@ -45,15 +46,43 @@ const updatePage = (valeur) => {
   }
 };
 
-const trierDate = (croissant) => {
+const trierDate = () => {
   sectionFiches.innerHTML = "";
-  const dateOrdre = Array.from(data.results);
+  const dateOrdre = Array.from(valeurDeData);
   dateOrdre.sort((a, b) => {
-    const dateA = new Date(a.firstdate_begin)
-    const dateB = new Date(b.firstdate_begin)
-    return dateA - dateB
+    const dateA = new Date(a.firstdate_begin);
+    const dateB = new Date(b.firstdate_begin);
+    return dateA - dateB;
   });
   updatePage(dateOrdre);
+};
+
+const daterange = () => {
+  const start = "2023-01-00";
+  const end = "2023-01-31";
+  const dateJanvier = valeurDeData.filter(
+    (date) => date.firstdate_begin >= start && date.firstdate_begin <= end
+  );
+  sectionFiches.innerHTML = "";
+  updatePage(dateJanvier);
+};
+
+const keyword = () => {
+  const saisi = document.getElementById("keyword").value;
+  const motsCle = valeurDeData.filter((mot) => mot.keywords_fr.includes(saisi));
+  sectionFiches.innerHTML = "";
+  updatePage(motsCle);
+  console.log(saisi);
+  console.log(motsCle);
+};
+
+const location = () => {
+  const region = document.getElementById("location").value;
+  const loc = valeurDeData.filter((reg) => reg.location_region === region);
+  sectionFiches.innerHTML = "";
+  updatePage(loc);
+  console.log(region);
+  console.log(loc);
 };
 
 const boutonTrierDate = document.getElementById("btn-date");
@@ -63,7 +92,30 @@ boutonTrierDate.addEventListener("click", () => {
 
 const boutonDefault = document.getElementById("btn-default");
 boutonDefault.addEventListener("click", () => {
-  updatePage(data.results);
+  updatePage(valeurDeData);
+});
+
+const boutonJanvier = document.getElementById("btn-janvier");
+boutonJanvier.addEventListener("click", () => {
+  daterange();
+});
+
+const motSaisi = document.getElementById("keyword");
+motSaisi.addEventListener("keydown", () => {
+  if (event.code === "Enter" || event.keyCode === 13) {
+    keyword();
+  }
+});
+
+const locationSaisi = document.getElementById("location");
+locationSaisi.addEventListener("keydown", () => {
+  if (event.code === "Enter" || event.keyCode === 13) {
+    location();
+  }
 });
 
 
+
+
+
+updatePage(valeurDeData);
